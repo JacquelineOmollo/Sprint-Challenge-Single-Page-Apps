5a41f5890a5fd36aeabe380a07c5758df4e2b339
Jacqueline Omollo <jacquelineOmolloo@gmail.com>
2019-08-18 21:59:34 -0400

    Commit page works. Now time for styling

---
+++ src/components/AppRouter.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Switch } from "react-router";
+import Header from "../components/Header";
+import WelcomePage from "../components/WelcomePage";
+import LocationsList from "../components/LocationsList";
+import CharacterList from "../components/CharacterList";
+import EpisodeList from "../components/EpisodeLists";
+
+const AppRouter = () => (
+  <div className="container">
+    <Header />
+    <Switch>
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/characters" component={CharacterList} />
+      <Route path="/locations" component={LocationsList} />
+      <Route path="/episodes" component={EpisodeList} />
+    </Switch>
+  </div>
+);
+
+export default AppRouter;
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,8 +1,26 @@
 import React from "react";
-import { Tab, Menu, Icon } from "semantic-ui-react";
+// import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
-// TODO: Add missing tabs below
-export default function TabNav() {
+const TabNav = () => (
+  <header>
+    <ul className="main-nav">
+      <li>
+        <NavLink exact to="/">
+          Home Page
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/characters">Characters</NavLink>
+      </li>
+      <li>
+        <NavLink to="/locations">Locations</NavLink>
+      </li>
+      <li>
+        <NavLink to="/episodes">Episodes</NavLink>
+      </li>
+    </ul>
+  </header>
+);
 
-};
+export default TabNav;
